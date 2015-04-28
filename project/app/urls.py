6ab287833dde3e4f0c0fab92d8fbc77a77d5eb9a
Saeed Jweiles <saeed13@meet.mit.edu>
2015-04-28 17:30:26 +0300

    hello world

--- project/app/urls.py
+++ project/app/urls.py
@@ -6,5 +6,6 @@ from app import views
 urlpatterns = patterns('',
     url(r'^home/$', views.home, name='home'),
     url(r'^$', views.index, name='index'),
-    url(r'^base/$', views.base, name='index'),
+    url(r'^base/$', views.base, name='base'),
+    url(r'^about/$', views.about, name='about'),
 )
--- project/app/views.py
+++ project/app/views.py
@@ -7,3 +7,5 @@ def home(request):
 	return render(request, 'app/home.html', {})
 def base(request):
 	return render(request, 'app/after_login_base.html', {})
+def about(request):
+	return render(request, 'app/about.html', {})
